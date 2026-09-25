15bf565e5a476c83dee2edcf4adf7ea318ac8eaf
isamoilov-creator <i.samoilov@g.nsu.ru>
2026-09-25 21:48:41 +0700

    Add files via upload

---
+++ untitled-20260917135915.py
@@ -0,0 +1,185 @@
+#1
+#print("Привет,Python!\nHello, Pyrhon\nBonjour Python!\nHej, Python\nHola, Python!")
+#2
+#print("Привет" " " "Python!")
+#3
+#s=int(input("Цена шоколадки: ") #r=int(input("Цена шоколадки2:"))
+#print(s+r)
+#4
+#print('(\___/)')
+#print("(='.'=)")
+#print('(")_(")')
+#5
+#print("Привет,Python!\nHello, Pyrhon\nBonjour Python!\nHej, Python\nHola, Python!")
+#6
+#print("Как вас зовут?")
+#i=input()
+#print("Здравствуйте, " + i)
+#print("Что вам нравится?")
+#s=input()
+#print("Отлично! " + s + " - хорошее увлечение.")
+#7
+# Запрос данных у пользователя
+#l = input("Login: ")
+#p = input("Password: ")
+#n_p = input("New password: ")
+#print(f"User {l} has changed the password to {n_p}")
+#8
+#print("Введите плей-лист папы:")
+#t1 = input()
+#t2 = input()
+#t3 = input()
+#t4 = input()
+#t5 = input()
+#print("Плей-лист мамы:")
+#print(t5)
+#print(t4)
+#print(t3)
+#print(t2)
+#print(t1)
+#9
+#n=input("Номер рейса: ")
+#na=input("Название авиакомпании: ")
+#gp=input("Город прилета: ")
+#gpa=input("Город прилета(англ): ")
+#print(f"Заканчивается посадка на рейс {n} авиокомпании {na} до {gp}")
+#print(f"This is the final boarding call for {na} {n} to {gpa}")
+#10
+#t= int(input())
+#sc= 96
+#gc= sc // 16
+#p= 48
+#st= sc * p
+#gt= t - st
+#gp= gt // gc
+#print(gp)
+#11
+#import math
+#r = int(input())
+#R = int(input())
+#s = math.pi * (R**2 - r**2)
+#print(s)
+#12
+#x = int(input())
+#y = ((((x + 2) * 3) - 6)// 3) - 4
+#print(y)
+#13
+#c = float(input())
+#i = c / 2.54
+#f = i / 12
+#y = f / 3
+#m = y / 1760
+#print(m, y, f, i, sep="\n")
+#14
+#s =input()
+#s1,s2= s.split()
+#print(s1, s2, sep="\n")
+#15
+#i = 1
+#m = 1
+#j = 7
+#c = j * 7
+#k = c * 7
+#m = k * 7
+#a = m + h + w + c + k
+#print(a)
+#16
+#v = float(input())
+#a = v * 0.19
+#print(a)
+#17
+#w= int(input())
+#v= int(input())
+#t=(w/v**2)*703
+#print(round(t,2))
+#18
+#a2= 10000 
+#hm1= int(input())  
+#hm= hm1 * 0.01
+#l3= 1000  
+#v= a2 * hm * l3
+#print(int(v))
+#19
+#n =int(input())
+#b=1
+#k=n+b
+#m=int(input())
+#p= m//k
+#print(p)
+#20
+#n = int(input())
+#k = int(input())
+#p = n%k
+#print(p)
+#21
+#m=float(input())
+#print(int(m//1609))
+#22
+#n=input()
+#p=n[-3]
+#print(p)
+#23
+#x=int(input())
+#h=x//3600
+#m=x%3600//60
+#s=x%60
+#print(f"{h} часов {m} минут {s} секунд")
+#24
+#if (int(input()))%2==0: print("0")
+#else: print("1")
+#25
+#x=int(input())
+#y=int(input())
+#n=int(input())
+#q=(x*100+y)*n
+#print(str(q)[0:-2] + " рублей")
+#print(str(q)[-2:] + " копеек")
+#26
+#n = 5
+#print(" ".join(["(\\___/)"] * n))
+#print(" ".join(["(='.'=)"] * n))
+#print(" ".join(["(\")_(\")"] * n))
+#27
+#k=input()
+#n=int(input())
+#r=int(input())
+#rf=int(k*n)*r
+#print(rf)
+#28
+#raw=input('Enter number: ')
+#num=raw
+#print(num)
+#31
+#x=int(input())
+#y=int(input())
+#if x//y==0 or y//x==0:
+#    print(1)
+#else:
+#    print(0)
+#32
+#n= float(input())
+#tm = int(n/0.5)
+#h = tm // 60
+#m = tm % 60
+#print(h, m)
+#33
+#n= int(input())
+#c= int(input())
+#k= int(input())
+#p= (k-1)//(n*c)+1
+#C = ((k-1) % (n*c))//n+1
+#R = (k-1)%n+1
+#print(f"страница {p} столбец {C} строка {R}")
+
+
+
+
+
+
+
+
+
+
+
+
+
